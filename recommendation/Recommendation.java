8ad378c0ce626a6cdfad8ee3e209411e2d1748b8
Youssef Bendiha <bendiha.youssef@gmail.com>
2022-11-04 16:00:48 -0400

    Add reduce logic
    * reduce logic of “People You Might Know"
    * filter and sort result
    * add the main function

--- recommendation/Recommendation.java
+++ recommendation/Recommendation.java
@@ -54,4 +54,87 @@ public class Recommendation {
       }
     }
   }
+
+  public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+  
+    // Initialize a hashmap that will contain the collected key,value 
+    HashMap<String, HashMap<String, Integer>> reduceMap = new HashMap<>();
+
+    public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter)
+        throws IOException {
+
+      // If the key does not exist in the reducemap, add the key with an empty map value 
+      if (reduceMap.get(key) == null)
+      {
+        reduceMap.put(key.toString(), new HashMap<>());  
+      }
+              
+      // Iterate through key values
+      while (values.hasNext()) {
+        // Split values by the delimter ',' to get into a list the potiential friend or friend with his flag value
+        String valueFlag[] = values.next().toString().split(",");
+        // Ensure that the list contains two values
+        if (valueFlag.length == 2) {
+          // Get the potential or friend value
+          String potentialFriend = valueFlag[0];
+          // If the key does not contain the potentialFriend as value key into the reduceMap, add it as new map with the flag value
+          if (!reduceMap.get(key.toString()).containsKey(potentialFriend))
+              reduceMap.get(key.toString()).put(potentialFriend, Integer.parseInt(valueFlag[1]));
+          else {
+              //Otherwise, update the existing value
+              int existingFlagValue = reduceMap.get(key.toString()).get(potentialFriend);
+              reduceMap.get(key.toString()).put(potentialFriend, existingFlagValue + Integer.parseInt(valueFlag[1]));
+            }
+          }
+      }
+
+      // Filter out values with negative flag since they are already friends with the user and sort remaining value in desc order
+      HashMap<String, Integer> sorted = reduceMap.get(key.toString())
+                                                   .entrySet()
+                                                   .stream()
+                                                   .filter(value -> value.getValue() > 0)
+                                                   .sorted(Collections.reverseOrder(java.util.Map.Entry.comparingByValue()))
+                                                   .collect(Collectors.toMap(java.util.Map.Entry::getKey, java.util.Map.Entry::getValue,(a, b) -> b, LinkedHashMap::new));
+    
+      //Update the key value
+      reduceMap.put(key.toString(), sorted);
+      
+      // Keep only the first 10 recommended users  
+      List<String> recommendedList = reduceMap.get(key.toString())
+                                          .keySet()
+                                          .stream()
+                                          .limit(10)
+                                          .collect(Collectors.toList());
+      
+      //Output the final datagrams with the recommendation list of users separated by ','
+      output.collect(key, new Text(String.join(",",recommendedList)));
+    }
+  }
+
+  // Main
+  public static void main(String[] args) throws Exception {
+    //Set up the haoop job
+    JobConf conf = new JobConf(Recommendation.class);
+    //Set the user-specified job name.
+    conf.setJobName("recommendation");
+
+    //Set the key/value class for the job output data.
+    conf.setOutputKeyClass(Text.class);
+    conf.setOutputValueClass(Text.class);
+
+    //Set the Mapper/reducer class for the job.
+    conf.setMapperClass(Map.class);
+    conf.setReducerClass(Reduce.class);
+
+    //Set the Input/Output format implementation for the map-reduce job.
+    conf.setInputFormat(TextInputFormat.class);
+    conf.setOutputFormat(TextOutputFormat.class);
+
+    //Set the array of Paths as the list of inputs/outputs for the map-reduce job
+    FileInputFormat.setInputPaths(conf, new Path(args[0]));
+    FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+    //Submit the job
+    JobClient.runJob(conf);
+  }
 }
